1f795b33ac193471b6e49cdc324771e4bf9b76de
Thomas <thomas.ostman at motrice.se>
2014-12-22 12:25:49 +0100

    Some functions for process listing and searching.

--- inherit-service/inherit-service-activiti-engine/src/test/java/org/inheritsource/test/service/processengine/ActivitiEngineServiceTest.java
+++ inherit-service/inherit-service-activiti-engine/src/test/java/org/inheritsource/test/service/processengine/ActivitiEngineServiceTest.java
@@ -27,13 +27,21 @@
 package org.inheritsource.test.service.processengine;
 
 import java.util.Locale;
+import java.util.List;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.inheritsource.service.processengine.ActivitiEngineService;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Ignore;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.inheritsource.service.common.domain.PagedProcessInstanceSearchResult;
+import org.inheritsource.service.common.domain.UserInfo;
+import org.inheritsource.service.processengine.ActivitiEngineUtil;
 
 public class ActivitiEngineServiceTest {
 
@@ -55,9 +63,164 @@ public class ActivitiEngineServiceTest {
 	}
 	
 	@Test
-	public void testProcessInstanceDetails() {
-		System.out.println(activitiEngineService.getProcessInstanceDetails("212", new Locale("sv", "SE")));
+	public void testRemoveProcess() {
+		System.out.println("testRemoveProcess()");
+	
+		activitiEngineService.getDeployedDeploymentIds() ; 
+		List <String> deployedDeploymentIds =   activitiEngineService.getDeployedDeploymentIds() ;
+	
+		 
+		for(String id:deployedDeploymentIds ) {
+			System.out.println("id = " + id ) ; 
+		}
+		
+//		 boolean cascade = true ; 
+//		String testid = "1701" ; 
+//			activitiEngineService.deleteDeploymentByDeploymentId(testid ,
+//                   cascade) ; 
+		
+		
+		ActivitiEngineUtil.listDeployedProcesses(activitiEngineService.getEngine()) ; 
+		// activitiEngineService.deleteDeploymentByDeploymentId(deploymentId, cascade);
+			System.out.println("testRemoveProcess() done");
 	}
+	
+	@Ignore
+	@Test
+	public void testProcessInstanceDetails() {
+
+		// String searchForUserId = "1";
+		String fromIndexStr = "0"; // start from element
+		String pageSizeStr = "10"; // nr of element per page
+		String sortBy = "started"; //
+		// String sortOrder = "desc"; // or "asc"
+		// String sortOrder = "asc" ;
+		String sortOrder = "desc";
+		// String filter = "STARTED" ; // "STARTED"; // or "FINISHED"
+		String filter = "FINISHED";
+		// maybe constant instead ???
+
+		// String filter = "FINISHED" ;
+		// String userIdRaw = "john";
+		String userIdRaw = "ranel@inherit.se";
+		// String userIdRaw = "197203732396";
+		// String userId = "john" ;
+
+		String startedByUserId = searchForBonitaUser(userIdRaw);
+		String userId = "john";
+
+		// startedByUserId = null;
+		// userId = null;
+
+		// String userId = "561e2ebc-4501-4a29-b6de-53749c723db1" ;
+
+		int fromIndex = Integer.parseInt(fromIndexStr);
+		int pageSize = Integer.parseInt(pageSizeStr);
+
+		Locale locale = new Locale("sv", "SE");
+		System.out.println("testProcessInstanceDetails: "
+				+ activitiEngineService.getProcessInstanceDetails("12801",
+						locale));
+
+
+
+
+
+
+		int tolDate = 10;
+		String involvedUserId;
+		involvedUserId = startedByUserId;
+		involvedUserId = userId;
+		startedByUserId = null;
+		startedByUserId = "admin";
+		involvedUserId = "john";
+		// involvedUserId = "ranel@inherit.se";
+		userId = "admin";
+		userId = null;
+		String sourceDate = "2014-09-18";
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+		Date date = null;
+		try {
+			date = format.parse(sourceDate);
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// date = null;
+		filter = "ALL";
+		filter = "STARTED";
+		int mycase = 3;
+		PagedProcessInstanceSearchResult result = null;
+		switch (mycase) {
+
+		case 0:
+			// komin advanced search 
+			result = activitiEngineService.getProcessInstancesAdvanced(
+					startedByUserId, involvedUserId, fromIndex, pageSize,
+					sortBy, sortOrder, filter, locale, userId, date, tolDate);
+			break;
+		
+		case 1: 
+//			result = 
+//		 activitiEngineService.getProcessInstancesByUuids(processInstanceIds,
+//		 fromIndex, pageSize, sortBy, sortOrder, filter, locale,
+//		 startedByUserId) ; 
+//			break;
+			
+		case 2: 	
+			 result =
+			  activitiEngineService.getProcessInstancesWithInvolvedUser(
+			  startedByUserId, fromIndex, pageSize, sortBy, sortOrder, filter,
+			  locale, userId) ;
+			
+			break;
+			
+		case 3: 	
+			// komin simple search
+			  result =
+			  activitiEngineService.getProcessInstancesStartedBy( startedByUserId,
+			  fromIndex, pageSize, sortBy, sortOrder, filter, locale, userId) ;
+			
+			break;
+			
+			
+			
+		}
+		
+		
+		
+		
+		System.out.println("result.getHits()         = " + result.getHits());
+		System.out.println("result.getNumberOfHits() = "
+				+ result.getNumberOfHits());
+		System.out.println("result.getFromIndex()    = "
+				+ result.getFromIndex());
+		System.out
+				.println("result.getPageSize()     = " + result.getPageSize());
+		System.out.println("result.getSortBy()       = " + result.getSortBy());
+		System.out.println("result.getSortOrder()    = "
+				+ result.getSortOrder());
+
+		// System.out.println("testProcessInstanceDetails: " +
+		// activitiEngineService.getUserInbox(locale, "admin"));
+	}
+
+	private String searchForBonitaUser(String searchForUserId) {
+		String searchForBonitaUser = searchForUserId;
+
+		if (searchForBonitaUser != null) {
+			searchForBonitaUser = searchForBonitaUser.trim();
+		}
+		UserInfo userInfo = activitiEngineService.getTaskFormDb()
+				.getUserBySerial(searchForBonitaUser);
+		if (userInfo != null) {
+			searchForBonitaUser = userInfo.getUuid();
+		}
+
+		return searchForBonitaUser;
+	}
+
+
 
 /*
 	@Test
