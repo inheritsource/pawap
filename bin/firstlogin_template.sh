2b9086a2ddab20fcee4f33c8f9b4d0349edab2ea
Thomas <thomas.ostman at motrice.se>
2015-05-12 16:19:01 +0200

    grep correction.

--- bin/firstlogin_template.sh
+++ bin/firstlogin_template.sh
@@ -53,8 +53,8 @@ fi
 #
 ## create database 
 ## get databasename and user from properties file 
-DATABASEUSERNAME=`grep "dataSource.username.=" ${MOTRICE_PROPERTIES} | cut -d = -f 2  | tr -d [:blank:]`
-DATABASENAME=`grep "dataSource.url.=" ${MOTRICE_PROPERTIES}  | cut -d \/ -f 4  | tr -d [:blank:]`
+DATABASEUSERNAME=`grep "^dataSource.username.=" ${MOTRICE_PROPERTIES} | cut -d = -f 2  | tr -d [:blank:]`
+DATABASENAME=`grep "^dataSource.url.=" ${MOTRICE_PROPERTIES}  | cut -d \/ -f 4  | tr -d [:blank:]`
 # cd ${PAWAP_DIR}/database/bin
 CREATE_USER="sudo  su postgres ./create_user.sh ${DATABASEUSERNAME}"
 CREATE_DATABASE="sudo  su postgres ./create_database.sh ${DATABASENAME} ${DATABASEUSERNAME}"
@@ -76,7 +76,7 @@ fi
 
 ## compile and deploy 
 ## get hostname from  properties file 
-HOSTNAME=`grep "site.base.uri=.*/site" /usr/local/etc/motrice/motrice.properties| cut -d / -f 3` 
+HOSTNAME=`grep "^site.base.uri=.*/site" /usr/local/etc/motrice/motrice.properties| cut -d / -f 3` 
 
 APPEND1='echo "ESERVICE_HOST=${HOSTNAME} " >> bin/config_deploy_minburk.sh'
 APPEND2='echo "MVN_SKIP_TEST=true" >> bin/config_deploy_minburk.sh' 
